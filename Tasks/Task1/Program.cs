921944290b45484b4c8f8856840980ae7a77df71
Batu YURT <asdasd019@hotmail.com>
2022-01-19 17:40:51 +0300

    .

--- Tasks/Task1/Program.cs
+++ Tasks/Task1/Program.cs
@@ -11,15 +11,33 @@ namespace Task1
         static void Main(string[] args)
         {
 
+            List<string> guzelFilmler = new List<string> { "Schindler's List", "Lotr Series", "Star Wars Series", "The Shawshank Redemption", "Fight Club" };
 
 
+            foreach (var film in guzelFilmler)
+            {
+                Console.WriteLine(film);
+            }
 
 
+            int enSevdigim = guzelFilmler.IndexOf("Lotr Series");
+            Console.WriteLine($"\nEn sevdiğim film kaçıncı indexte : {enSevdigim}\n");
 
 
+            Console.WriteLine("Matrix 4 guzelFilmler listesinde yer alıyor mu");
+            bool matrix = guzelFilmler.Contains("Matrix 4");
+            Console.WriteLine(matrix);
 
 
+            string fightClub = "\n\nBen sürekli bug içeren kod yazıyorsam, bir adam sürekli kodlarımı düzeltiyorsa...";
 
+            string[] ayrilmis = fightClub.Split(',');
+            foreach (var kelime in ayrilmis)
+            {
+                Console.WriteLine(kelime);
+            }
+
+            Console.ReadLine();
         }
     }
 }

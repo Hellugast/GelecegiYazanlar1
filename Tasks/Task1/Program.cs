c54234855bf9ecbcad8492979bc523825a7d5b24
Batu YURT <asdasd019@hotmail.com>
2022-01-19 17:41:59 +0300

    .

--- Tasks/Task1/Program.cs
+++ Tasks/Task1/Program.cs
@@ -19,11 +19,11 @@ namespace Task1
                 Console.WriteLine(film);
             }
 
-
+            //indexof
             int enSevdigim = guzelFilmler.IndexOf("Lotr Series");
             Console.WriteLine($"\nEn sevdiğim film kaçıncı indexte : {enSevdigim}\n");
 
-
+            //contains
             Console.WriteLine("Matrix 4 guzelFilmler listesinde yer alıyor mu");
             bool matrix = guzelFilmler.Contains("Matrix 4");
             Console.WriteLine(matrix);
@@ -31,6 +31,8 @@ namespace Task1
 
             string fightClub = "\n\nBen sürekli bug içeren kod yazıyorsam, bir adam sürekli kodlarımı düzeltiyorsa...";
 
+
+            //split
             string[] ayrilmis = fightClub.Split(',');
             foreach (var kelime in ayrilmis)
             {

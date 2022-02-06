a37e25700a49ade1ab6e4c16a07a74c5c2ae3278
Batu YURT <asdasd019@hotmail.com>
2022-02-06 03:00:37 +0300

    ..

--- Tasks/TaskWarSimulator/Weapon.cs
+++ Tasks/TaskWarSimulator/Weapon.cs
@@ -8,5 +8,40 @@ namespace TaskWarSimulator
 {
     class Weapon
     {
+        public int Damage { get; set; }
+        public string weaponName { get; set; }
     }
+
+    class Knife : Weapon
+    {
+        public Knife()
+        {
+            this.weaponName = "knife";
+            this.Damage = 11;
+        }
+    }
+
+    class Rifle : Weapon
+    {
+        public Rifle()
+        {
+            this.weaponName = "Rifle";
+            this.Damage = 15;
+        }
+
+    }
+
+    class LMG : Weapon
+    {
+        public LMG()
+        {
+            this.weaponName = "LMG";
+            this.Damage = 20;
+        }
+    }
+
+
+
+
+
 }
